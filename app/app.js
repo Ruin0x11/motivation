a019db82626db0c021b9da2721ccb724d72c7067
Ian Pickering <ipickering2@gmail.com>
2016-10-02 23:09:54 -0600

    sigh

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+
+
 (function(){
     var $  = document.getElementById.bind(document);
     var $$ = document.querySelectorAll.bind(document);
@@ -7,8 +9,8 @@
         this.load();
 
         this.$el.addEventListener(
-            'submit', this.submit.bind(this)
-        );
+                'submit', this.submit.bind(this)
+                );
 
         if (this.dob) {
             this.renderAgeLoop();
@@ -19,20 +21,90 @@
 
     App.fn = App.prototype;
 
+    App.fn.midnight = function() {
+        var midnight = new Date();
+        midnight.setHours( 24 );
+        midnight.setMinutes( 0 );
+        midnight.setSeconds( 0 );
+        midnight.setMilliseconds( 0 );
+        return midnight;
+    }
+
+    App.fn.nextHour = function() {
+        var date = new Date();
+        var minutes = 60 - date.getMinutes();
+        return new Date(date.getTime() + 60000 * minutes)
+    }
+
+    App.fn.loadMotivators = function() {
+        var value;
+    }
+
     App.fn.load = function(){
         var value;
+
+        this.wallpapers = localStorage['wallpapers'].split('\n');
+        this.motivator = {"item":"", "author":""}
 
         if (value = localStorage.dob)
             this.dob = new Date(parseInt(value));
 
-        this.wallpapers = localStorage['wallpapers'].split('\n');
-        this.wallpaper = this.wallpapers.length == 0 ? '' : this.wallpapers[0];
-        $$('body')[0].style.backgroundImage = 'url(' + this.wallpaper + ')';
+        if (value = localStorage.changeDate)
+            this.changeDate = new Date(parseInt(value))
+        else
+            this.changeDate = new Date(0)
+                if (value = localStorage.wallpaperIdx) {
+                    if (value >= this.wallpapers.length)
+                        this.wallpaperIdx = 0
+                    else
+                        this.wallpaperIdx = value;
+                }
+
+
+        if (value = localStorage.motivatorIdx) { 
+            if (value >= motivators.length)
+                this.motivatorIdx = 0;
+            else
+                this.motivatorIdx = value;
+        }
+        else
+            this.motivatorIdx = 0;
+        var motivators = [ {
+                "body": "Losers have inspiration, productive people have systems.",
+                "author": "guy"
+            }, {
+                "body" : "Part of growing up is learning to prioritize what you need to do, even if it isn't fun, over what you like to do.",
+                "author" : "bane"
+            }, {
+                "body": "A man must choose a path which will let his Abilities function at maximum efficiency toward the gratification of his Desires. ",
+                "author": "Hunter S. Thomspon"} ] 
+
+
+        if(this.changeDate.getTime() - new Date().getTime() < 0) {
+            this.motivatorIdx = Math.floor(Math.random()*motivators.length);
+            this.wallpaperIdx = Math.floor(Math.random()*this.wallpapers.length);
+        }
+
+        this.motivator = motivators.length == 0 ? {"body":"", "author":""} : motivators[this.motivatorIdx];
+        $$('.motivator-body')[0].innerHTML = this.motivator.body;
+        $$('.motivator-author')[0].innerHTML = this.motivator.author;
+
+        this.loadMotivators();
+
+        this.wallpaper = this.wallpapers.length == 0 ? '' : this.wallpapers[this.wallpaperIdx];
+
+        $$('body')[0].style.backgroundImage = "url(" + this.wallpaper + ")";
     };
 
     App.fn.save = function(){
         if (this.dob)
             localStorage.dob = this.dob.getTime();
+        if(this.changeDate) {
+            if(this.changeDate.getTime() - new Date().getTime() < 0) {
+                this.wallpaperIdx = Math.floor(Math.random()*this.wallpapers.length);
+                localStorage.changeTime = this.changeTime.getTime();
+            }
+        }
     };
 
     App.fn.submit = function(e){
@@ -55,7 +127,7 @@
     };
 
     App.fn.renderAge = function(){
-        var now       = new Date
+        var now       = new Date;
         var duration  = now - this.dob;
         var years     = duration / 31556900000;
 
